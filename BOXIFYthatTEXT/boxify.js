ad97ead20c406b275626ab4a85de0698b45544a9
mzardab <zardab12@hotmail.com>
2016-03-13 19:04:44 +0000

    Rewrote approach with DOM
    Rewrote how box, addbutton and list output are written. List output now
    works.

--- BOXIFYthatTEXT/boxify.js
+++ BOXIFYthatTEXT/boxify.js
@@ -4,17 +4,63 @@ datastructure to save list
 displaying list to page/deleting specific elements
 submit button to convert to array
 */
-function newBox(id)
+
+//GLOBAL CONFIG VAR
+function newBox(targetId, boxId)
   {
-    var listItems= []; //Holds the list before submission
+    var buttonId = boxId + "-button"; //creates unique signifier for button
+    var listId = boxId + "-list"; //creates unique signifier for list
 
-    document.getElementById(id).innerHTML = "<input type='text' id='boxify-textbox' name='"+id+"-textbox'/>";
-    console.log("test");
+    var box = document.createElement("INPUT"); //create textbox
+    box.setAttribute("type", "text");
+    box.setAttribute("value", "test");
+    box.setAttribute("class", "boxify-text-box")
+    box.setAttribute("name", boxId);
+    box.setAttribute("id", boxId);
+
+    var addButton = document.createElement("BUTTON"); //creates add button
+    addButton.setAttribute("id", buttonId);
+    addButton.setAttribute("type", "BUTTON");
+    addButton.setAttribute("value", "ADD");
+    addButton.setAttribute("class", "boxify-text-add-button")
+    addButton.setAttribute("parent-text-box", targetId);
+
+    var listWrapper = document.createElement("DIV");
+    listWrapper.setAttribute("id", listId);
+
+    document.getElementById(targetId).appendChild(box);
+    document.getElementById(targetId).appendChild(addButton);
+    document.getElementById(targetId).appendChild(listWrapper);
 
 
+
+
+    //Defining listener for Add BUTTON
+    document.getElementById(buttonId).onclick = function() {
+
+    var text = document.getElementById(boxId).value;
+    var list = document.createElement("LI");
+    list.setAttribute("class", "willy");
+    list.setAttribute("value", text);
+
+    list.innerHTML = text;
+    //var li = "<li>" + text + "</li>";
+    document.getElementById(listId).appendChild(list);
+}
 };
+
+function listElement(parentBox, data)
+{
+
+}
+
+function listElementTemplate() //list elements for deletion
+{
+
+}
 
 function submitForm()
 {
+  //use nodelist to submit to array?
   var outputArray = listItems;
 };
